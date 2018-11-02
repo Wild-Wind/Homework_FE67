a3ea23482e729e5180a5e8dd9d4d9662682a1a7a
Vladimir.M <vladimir.manin@yahoo.com>
2018-11-03 00:51:06 +0200

    Task 2 of HW_Lesson4

--- HW_Lesson4/main_hw3.js
+++ HW_Lesson4/main_hw3.js
@@ -1,36 +1,38 @@
 // Part 1. numPrompt 
-var numPrompt = function () {
-    var x = prompt('Input something', '');
-        if (x === null) {
-            return x;
-        }
-        if ((x >= 0) || (x < 0)) {
-            return x;
-        } 
-        return "NaN";  
-}
-console.log(numPrompt());
-
-// Part 2. myCalc (в работе)
-// var myCalc = function () {
-//     var x1 = prompt('Input first number', 'It must be only number.');
-//     var x2 = prompt('Input second number', 'It must be only number.');
-//     var x3 = prompt('Input symbol', 'Only "+" or "-"');
-//     console.log(x1);
-//     console.log(x2);
-//     console.log(x3);
-//         if (x1 || x2 || x3 === null) {
-//             return "Cancel";
-//         }
-//         if (x1 * x2 === NaN) {
-//             return "Inputed uncorrect data"; 
+// var numPrompt = function () {
+//     var x = prompt('Input something', '');
+//         if (x === null) {
+//             return x;
 //         }
 //         if ((x >= 0) || (x < 0)) {
 //             return x;
 //         } 
 //         return "NaN";  
 // }
-// console.log(myCalc());
+// console.log(numPrompt());
+
+// Part 2. myCalc
+var myCalc = function () {
+    var x1 = prompt('Input first number', 'It must be only number.');
+        if (((x1 >= 0) && (x1 != null)) || (x1 < 0)) {
+          console.log(x1);
+          var x2 = prompt('Input second number', 'It must be only number.');
+        if (((x2 >= 0) && (x2 != null)) || (x2 < 0)) {
+          console.log(x2);
+          var x3 = prompt('Input symbol', 'Only "+" or "-"');
+        if ((x3 === '-') || (x3 === '+')) {
+            console.log(x3);
+          if (x3 === '-') {
+            return (+x1 + +x2) * -1;
+          } else {
+                    return (+x1 + +x2);
+            } 
+        }        
+        } 
+        } 
+        return 'You choiced CANCEL or Inputed uncorrect data';
+}
+console.log(myCalc());
 
 // Part 3. mySlice (в работе)
 // var arr = [30, 51, 74, 106, 540, 356, 280, 859, 372];
